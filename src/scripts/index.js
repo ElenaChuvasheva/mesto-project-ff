3252e17f9b24810eaa0bc3efcd3a0a3f0f264d8d
Елена Чувашева <chuvashevalena@yandex.ru>
2024-01-24 19:20:59 +0300

    2nd review

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,11 +7,11 @@ import { closeModal, handleOverlayClick, openModal } from "./modal.js";
 const placesList = document.querySelector(".places__list");
 const imgPopup = document.querySelector(".popup_type_image"),
   profilePopup = document.querySelector(".popup_type_edit"),
-  addPopup = document.querySelector(".popup_type_new-card");
+  newPlacePopup = document.querySelector(".popup_type_new-card");
 const openProfilePopupButton = document.querySelector(".profile__edit-button"),
   openAddPopupButton = document.querySelector(".profile__add-button");
-const popupImg = imgPopup.querySelector(".popup__image");
-const popupImgCaption = imgPopup.querySelector(".popup__caption");
+const photoImgPopup = imgPopup.querySelector(".popup__image");
+const captionImgPopup = imgPopup.querySelector(".popup__caption");
 const profileForm = document.querySelector("[name='edit-profile']");
 const profileTitle = document.querySelector(".profile__title");
 const profileDescription = document.querySelector(".profile__description");
@@ -19,47 +19,41 @@ const newPlaceForm = document.querySelector("form[name='new-place']");
 const newPlaceName = newPlaceForm.querySelector("input[name='place-name']");
 const newPlaceUrl = newPlaceForm.querySelector("input[name='link']");
 
-const zoomPhoto = (event) => {
-  if (event.target.classList.contains("card__image")) {
-    popupImgCaption.textContent = event.target
-      .closest(".card")
-      .querySelector(".card__title").textContent;
-    popupImg.src = event.target.src;
-    popupImg.alt = event.target.alt;
-  }
+const zoomPhoto = (title, src) => {
+  captionImgPopup.textContent = title;
+  photoImgPopup.src = src;
+  photoImgPopup.onload = () => {
+    openModal(imgPopup);
+  };
 };
 
 const handleProfileFormSubmit = (event) => {
   event.preventDefault();
   profileTitle.textContent = profileForm.name.value;
   profileDescription.textContent = profileForm.description.value;
-  closeModal(event.target.closest(".popup"));
+  closeModal(profilePopup);
 };
 
 const handleNewPlaceFormSubmit = (event) => {
   event.preventDefault();
-  closeModal(event.target.closest(".popup"));
   const cardData = { name: newPlaceName.value, link: newPlaceUrl.value };
-  const newCardClone = makeCard(cardData);
+  const newCardClone = makeCard(
+    cardData,
+    deleteCard,
+    handleLikeButton,
+    zoomPhoto
+  );
   placesList.prepend(newCardClone);
-  closeModal(event.target.closest(".popup"));
+  closeModal(newPlacePopup);
   newPlaceForm.reset();
 };
 
 initialCards.forEach((item) => {
-  placesList.append(makeCard(item));
+  placesList.append(makeCard(item, deleteCard, handleLikeButton, zoomPhoto));
 });
 
-popupImg.onload = () => {
-  openModal(imgPopup);
-};
-
-[handleLikeButton, deleteCard, zoomPhoto].forEach((callback) => {
-  placesList.addEventListener("click", callback);
-});
-
-[imgPopup, profilePopup, addPopup].forEach((popup) => {
-  let closePopupButton = popup.querySelector(".popup__close");
+[imgPopup, profilePopup, newPlacePopup].forEach((popup) => {
+  const closePopupButton = popup.querySelector(".popup__close");
   closePopupButton.addEventListener("click", () => {
     closeModal(popup);
   });
@@ -76,7 +70,7 @@ openProfilePopupButton.addEventListener("click", () => {
 profileForm.addEventListener("submit", handleProfileFormSubmit);
 
 openAddPopupButton.addEventListener("click", () => {
-  openModal(addPopup);
+  openModal(newPlacePopup);
 });
 
 newPlaceForm.addEventListener("submit", handleNewPlaceFormSubmit);
